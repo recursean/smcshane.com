e4503b4b3fb8f5923b7c8024caba93fad6974ff8
Sean McShane <seanmcshane96@gmail.com>
2022-07-08 03:06:31 +0000

    Refactoring for express nodejs server

--- server.js
+++ server.js
@@ -1,15 +1,141 @@
-const http = require("http");
+/**
+ * This is the main web server for smcshane.com
+ * 
+ * Run as long-running task with "nohup npm start &"
+ */
 
-// host and port to listen for incoming requests
-const port = 80;
+const express = require('express')
+const server = express()
+const fs = require('fs').promises;
+const port = 80
 
-const requestListener = function(req, res) {
-    res.writeHead(200);
-    console.log('Request received');
-    res.end("Hello world!");
-};
+// arphotoview-privacy.html
+server.get('/arphotoview-privacy', (req, res) => {
+    fs.readFile(__dirname + "/html/arphotoview-privacy.html")
+        .then(contents => {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+        })
+        .catch(err => {
+            res.writeHead(500);
+            res.end(err);
+            return;
+        });
+})
 
-const server = http.createServer(requestListener);
+// arphotoview.html
+server.get('/arphotoview', (req, res) => {
+    fs.readFile(__dirname + "/html/arphotoview.html")
+        .then(contents => {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+        })
+        .catch(err => {
+            res.writeHead(500);
+            res.end(err);
+            return;
+        });
+})
+
+// contact.html
+server.get('/contact', (req, res) => {
+    fs.readFile(__dirname + "/html/contact.html")
+        .then(contents => {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+        })
+        .catch(err => {
+            res.writeHead(500);
+            res.end(err);
+            return;
+        });
+})
+
+// index.html
+server.get('/', (req, res) => {
+    fs.readFile(__dirname + "/html/index.html")
+        .then(contents => {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+        })
+        .catch(err => {
+            res.writeHead(500);
+            res.end(err);
+            return;
+        });
+})
+
+// newscloud.html
+server.get('/newscloud', (req, res) => {
+    fs.readFile(__dirname + "/html/newscloud.html")
+        .then(contents => {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+        })
+        .catch(err => {
+            res.writeHead(500);
+            res.end(err);
+            return;
+        });
+})
+
+// saf.html
+server.get('/saf', (req, res) => {
+    fs.readFile(__dirname + "/html/saf.html")
+        .then(contents => {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+        })
+        .catch(err => {
+            res.writeHead(500);
+            res.end(err);
+            return;
+        });
+})
+
+// upick-privacy.html
+server.get('/upick-privacy', (req, res) => {
+    fs.readFile(__dirname + "/html/upick-privacy.html")
+        .then(contents => {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+        })
+        .catch(err => {
+            res.writeHead(500);
+            res.end(err);
+            return;
+        });
+})
+
+// upick.html
+server.get('/upick', (req, res) => {
+    fs.readFile(__dirname + "/html/upick.html")
+        .then(contents => {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+        })
+        .catch(err => {
+            res.writeHead(500);
+            res.end(err);
+            return;
+        });
+})
+
+// Allow static content to be automatically served from resources directory.
+// The below causes 'static' to not be part of the URL when static content
+// is requested. So, when requesting static content from HTML pages, do not
+// include static/ in path, instead start at resources/
+server.use(express.static('static'))
+
+// Start server
 server.listen(port, () => {
-    console.log(`smcshane.com is running on ${port}`);
-});
+    console.log(`smcshane.com running on port ${port}`)
+})
